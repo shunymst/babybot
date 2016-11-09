a6c428c3d2f85e888209b94c25d4639129838ebc
shunymst <shunjiymst@gmail.com>
2016-11-09 10:50:08 +0900

    line api update

--- bot.py
+++ bot.py
@@ -14,19 +14,16 @@ handler.setLevel(DEBUG)
 logger.setLevel(DEBUG)
 logger.addHandler(handler)
 
-# ENDPOINT_URI = 'https://trialbot-api.line.me/v1/events'
-ENDPOINT_URI = 'https://api.line.me/v2/bot/message/reply'
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 DOCOMO_API_KEY = os.environ.get('DOCOMO_API_KEY', '')
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN', '')
 
 
 class CallbackResource(object):
     # line
     header = {
         'Content-Type': 'application/json; charset=UTF-8',
-#        'X-Line-ChannelID': os.environ['LINE_CHANNEL_ID'],
-#        'X-Line-ChannelSecret': os.environ['LINE_CHANNEL_SECRET'],
-#        'X-Line-Trusted-User-With-ACL': os.environ['LINE_CHANNEL_MID'],
-        'Authorization': 'Bearer {}'.format(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
+        'Authorization': 'Bearer {}'.format(LINE_CHANNEL_ACCESS_TOKEN)
     }
 
     # docomo baby
@@ -74,7 +71,7 @@ def on_post(self, req, resp):
             send_content = json.dumps(send_content)
             logger.debug('send_content: {}'.format(send_content))
 
-            res = requests.post(ENDPOINT_URI, data=send_content, headers=self.header)
+            res = requests.post(REPLY_ENDPOINT, data=send_content, headers=self.header)
             logger.debug('res: {} {}'.format(res.status_code, res.reason))
 
             resp.body = json.dumps('OK')
